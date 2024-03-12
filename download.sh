0b3ceaa03c68befd997be422afc36a2b6db5c193
victorvieirat <usuario@usuario>
2024-03-12 09:42:09 -0300

    Commit inicial

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+data_folder="data"
+link="https://cdn.tse.jus.br/estatistica/sead/eleicoes/eleicoes2022/arqurnatot/bu_imgbu_logjez_rdv_vscmr_2022_1t_RN.zip"
+mkdir -p $data_folder
+wget  $link  -P $data_folder/
+
