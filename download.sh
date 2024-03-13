df784cb06488db0556ebbd7a3010673207ceab67
Victor Vieira Targino <victorvieira.rn@gmail.com>
2024-03-13 18:39:51 -0300

    df com coluna id eleitor

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-data_folder="data"
+data_folder="data/zip"
 link="https://cdn.tse.jus.br/estatistica/sead/eleicoes/eleicoes2022/arqurnatot/bu_imgbu_logjez_rdv_vscmr_2022_1t_RN.zip"
 mkdir -p $data_folder
 wget  $link  -P $data_folder/
